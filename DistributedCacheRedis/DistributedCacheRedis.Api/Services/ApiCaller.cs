611a3ab4f0b00b6761a4a0452f8fe1188bf5b014
j-ew-s <gabriel.scavassa@outlook.com>
2017-09-30 17:08:03 -0300

    Creating Api Caller class

---
+++ DistributedCacheRedis/DistributedCacheRedis.Api/Services/ApiCaller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DistributedCacheRedis.Api.Services
+{
+    public static class ApiCaller
+    {
+        public static async Task<string> GetPost()
+        {
+            var http = new HttpClient();
+            var url = String.Format("https://jsonplaceholder.typicode.com/posts");
+            var response = await http.GetAsync(url);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
