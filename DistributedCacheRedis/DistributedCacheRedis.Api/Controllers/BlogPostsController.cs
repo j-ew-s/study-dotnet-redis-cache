5b2bd4436952312850403861dd422cb59056c18f
j-ew-s <gabriel.scavassa@outlook.com>
2017-09-30 17:50:52 -0300

    Creating Blog Post - method

--- DistributedCacheRedis/DistributedCacheRedis.Api/Controllers/BlogPostsController.cs
+++ DistributedCacheRedis/DistributedCacheRedis.Api/Controllers/BlogPostsController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DistributedCacheRedis.Api.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,11 +13,40 @@ namespace DistributedCacheRedis.Api.Controllers
     [Route("api/[controller]")]
     public class BlogPostsController : Controller
     {
+        private readonly IDistributedCache _distributedCache;
+        private readonly DistributedCacheEntryOptions _cacheOptions;
+        private readonly string _cacheKey = "BlogPost";
+            
+
+        public BlogPostsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+
+            _cacheOptions = new DistributedCacheEntryOptions()
+            {
+                AbsoluteExpirationRelativeToNow = (DateTime.Now.AddMinutes(1) - DateTime.Now)
+            };
+
+        }
         // GET: api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<string>  Get()
         {
-            return new string[] { "value1", "value2" };
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var cachedData = await _distributedCache.GetStringAsync(_cacheKey);
+
+            if (string.IsNullOrEmpty(cachedData))
+            {
+                await _distributedCache.SetStringAsync(_cacheKey, cachedData, _cacheOptions);
+            }
+            else
+            {
+                watch.Stop();
+                return "CACHED. You took " + watch.ElapsedMilliseconds + " Milliseconds to have your response. Post  : " + cachedData;
+            }
+            watch.Stop();
+            return "NO CACHED data. You took " + watch.ElapsedMilliseconds + " Milliseconds to have your response. The result from API (Post): " + cachedData;
+
         }
 
         // GET api/values/5
